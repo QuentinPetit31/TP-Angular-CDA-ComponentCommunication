734a57ba0fa4b1df84fc1341b741cd32f42d90af
QuentinPetit31 <140169105+QuentinPetit31@users.noreply.github.com>
2025-03-06 16:57:15 +0100

    Init repo

---
+++ TP-Angular-CDA-ComponentCommunication/src/app/components/user-display/user-display.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-display',
+  imports: [],
+  template: `
+    <p>
+      user-display works!
+    </p>
+  `,
+  styles: ``
+})
+export class UserDisplayComponent {
+
+}
